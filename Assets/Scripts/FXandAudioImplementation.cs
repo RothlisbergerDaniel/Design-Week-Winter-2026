416b31b900446530d00153fe787f81fdb3845935
Brandon <brandonhutchison6@gmail.com>
2026-03-04 13:43:00 -0500

    Added Inhale and Exhale Sound Effects

--- Assets/Scripts/FXandAudioImplementation.cs
+++ Assets/Scripts/FXandAudioImplementation.cs
@@ -5,7 +5,10 @@ public class FXandAudioImplementation : MonoBehaviour
     [SerializeField] GameObject suck;
     [SerializeField] ParticleSystem blow;
     [SerializeField] GameObject cam;
+    [SerializeField] AudioSource playerAudio;
+    [SerializeField] AudioClip[] audios;
     public bool sucking;
+    bool suckingLoop;
 
     // Update is called once per frame
     void Update()
@@ -20,9 +23,32 @@ public class FXandAudioImplementation : MonoBehaviour
             {
                 suck.SetActive(true);
             }
+
+            if (!suckingLoop)
+            {
+                playerAudio.clip = audios[0];
+                playerAudio.Play();
+                suckingLoop = true;
+            }
+            else if (!playerAudio.isPlaying)
+            {
+                playerAudio.loop = true;
+                playerAudio.clip = audios[1];
+                playerAudio.Play();
+            }
+            
         }
         else
         {
+            if (suckingLoop)
+            {
+                playerAudio.loop = false;
+                playerAudio.clip = audios[2];
+                playerAudio.Play();
+                suckingLoop = false;
+            }
+
+
             if (suck.gameObject.activeSelf)
             {
                 suck.SetActive(false);
@@ -32,6 +58,14 @@ public class FXandAudioImplementation : MonoBehaviour
     }
     public void Blow()
     {
-            blow.Play();
+        sucking = false;
+        suckingLoop = false;
+
+        int currentScream = Random.Range(3, 5);
+        playerAudio.loop = false;
+        playerAudio.clip = audios[currentScream];
+        playerAudio.Play();
+
+        blow.Play();
     }
 }
